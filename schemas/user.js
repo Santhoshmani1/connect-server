4413a8fa884d5dbb957494bb86b4b22364ec34e8
Santhosh Mani <pitakasanthosh@gmail.com>
2023-12-25 18:42:23 +0530

    Add user schema definition

---
+++ schemas/user.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+import { projectSchema } from "./projects.js";
+import { postsSchema } from "./posts.js";
+
+export const userSchema = mongoose.Schema({
+  name: String,
+  email: {
+    type: "String",
+    unique: true,
+  },
+  password: String,
+  skills: [String],
+  projects: [projectSchema],
+  about: {
+    type: "String",
+    default: "",
+  },
+  profilePic: {
+    type: "String",
+    default: "",
+  },
+  posts: {
+    type : [postsSchema],
+    default : []
+  },
+  github: {
+    type: "String",
+    default: "",
+  },
+  twitter: {
+    type: "String",
+    default: "",
+  },
+  linkedin: {
+    type: "String",
+    default: "",
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
